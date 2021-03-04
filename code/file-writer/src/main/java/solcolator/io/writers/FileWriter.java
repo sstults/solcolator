a051613bead88e9fe289d86dc51cf134d5613ab3
Scott Stults <sstults@o19s.com>
2021-03-04 06:35:54 -0500

    Let the FileOutputStream throw the IOException if the specified file cannot be created

--- code/file-writer/src/main/java/solcolator/io/writers/FileWriter.java
+++ code/file-writer/src/main/java/solcolator/io/writers/FileWriter.java
@@ -1,7 +1,14 @@
 package solcolator.io.writers;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.solr.common.SolrInputDocument;
+import org.apache.solr.common.util.NamedList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import solcolator.io.api.ISolcolatorResultsWriter;
+
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -9,20 +16,9 @@ import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-
-import org.apache.solr.common.SolrInputDocument;
-import org.apache.solr.common.util.NamedList;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import solcolator.io.api.ISolcolatorResultsWriter;
+import java.util.Objects;
 
 /**
- * NOTE! IT'S A BASIC IMPLEMENTATION ONLY. DON'T USE THIS WRITER IN PRODUCTION ENVIRONMENT WITHOUT ADDITIONAL TESTS!
- * 
  * This writer is designed to write solcolator results to a file
  * This writer can be used for testing and integration purposes
  * 
@@ -34,27 +30,25 @@ import solcolator.io.api.ISolcolatorResultsWriter;
 	</lst>
  */
 public class FileWriter implements ISolcolatorResultsWriter {
-	private static final Logger log = LoggerFactory.getLogger(FileWriter.class);
-	private static final String FILE_PATH = "filePath";
-	private static final String FILE_FL = "fileFl";
+	public static final String FILE_PATH = "filePath";
+	public static final String FILE_FL = "fileFl";
+
 	private final Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+	private static final Logger log = LoggerFactory.getLogger(FileWriter.class);
 
 	private List<String> fl;
 	private BufferedWriter bw;
 	
 	public void init(NamedList<?> outputConfig) throws IOException {
-		this.fl = Arrays.asList(((String) outputConfig.get(FILE_FL)).split(","));
+		String flString = (String) outputConfig.get(FILE_FL);
 		String filePath = (String) outputConfig.get(FILE_PATH);
-		File file = new File(filePath);
-		
-		if (!file.exists()) {
-			try {
-				new File(filePath).createNewFile();
-			} catch (IOException e) {
-				throw new IOException(String.format("File %s doesn't exist and can't be created", filePath));
-			}
+
+		if (Objects.isNull(flString) || Objects.isNull(filePath)) {
+			throw new IllegalArgumentException(String.format("%s and %s must be configured, but were [%s] and [%s]",
+				FILE_PATH, FILE_FL, filePath, flString));
 		}
-				
+
+		fl = Arrays.asList(flString.split(","));
 		bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filePath, false), StandardCharsets.UTF_8));
 	}
 	
