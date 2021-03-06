d753f60dcc75d0197270966a4f7c474a5fcd5313
Scott Stults <sstults@o19s.com>
2021-03-06 14:08:24 -0500

    Use LogCaptor to check expected log entries in unit tests

--- code/file-writer/src/test/java/solcolator/io/writers/FileWriterTest.java
+++ code/file-writer/src/test/java/solcolator/io/writers/FileWriterTest.java
@@ -2,9 +2,11 @@ package solcolator.io.writers;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import nl.altindag.log.LogCaptor;
 import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.common.util.NamedList;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
@@ -20,10 +22,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 import static solcolator.io.writers.FileWriter.FILE_FL;
 import static solcolator.io.writers.FileWriter.FILE_PATH;
 
@@ -32,6 +37,25 @@ class FileWriterTest {
   @TempDir File tmpDir;
   private static final String FILENAME = "testOutput.json";
   private static final String FIELD_LIST = "id,name,category";
+  private static final Map<String, List<SolrInputDocument>> QUERIES_TO_DOCS = new HashMap<>();
+  private static final int NUM_TEST_QUERIES = 3;
+  private static final int NUM_TEST_MATCHES = 3;
+
+  @BeforeAll
+  static void setup() {
+    var fields = FIELD_LIST.split(",");
+    for (int i = 1; i <= NUM_TEST_QUERIES; i++) {
+      List<SolrInputDocument> list = new ArrayList<>();
+      for (int j = 1; j <= NUM_TEST_MATCHES; j++) {
+        var doc = new SolrInputDocument();
+        for (String field : fields) {
+          doc.addField(field, field + i*j);
+        }
+        list.add(doc);
+      }
+      QUERIES_TO_DOCS.put("query" + i, list);
+    }
+  }
 
   @AfterEach
   void teardown() {
@@ -41,7 +65,7 @@ class FileWriterTest {
   }
 
   @Test
-  void shouldOpenFileForWriting() throws IOException {
+  void shouldOpenFileForWriting() {
     var path = Path.of(tmpDir.getAbsolutePath(), FILENAME);
     assertFalse(Files.exists(path));
     createTestFileWriter();
@@ -49,7 +73,7 @@ class FileWriterTest {
   }
 
   @Test
-  void shouldMaintainFieldList() throws IOException {
+  void shouldMaintainFieldList() {
     var fileWriter = createTestFileWriter();
     assertEquals(Arrays.asList(FIELD_LIST.split(",")), fileWriter.getFl());
   }
@@ -79,42 +103,38 @@ class FileWriterTest {
 
   @Test
   void writeSolcolatorResults() throws IOException {
-    Map<String, List<SolrInputDocument>> queriesToDocs = new HashMap<>();
-    var fields = FIELD_LIST.split(",");
-    int numTestQueries = 3;
-    int numTestMatches = 3;
-
-    for (int i = 1; i <= numTestQueries; i++) {
-      List<SolrInputDocument> list = new ArrayList<>();
-      for (int j = 1; j <= numTestMatches; j++) {
-        var doc = new SolrInputDocument();
-        for (String field : fields) {
-          doc.addField(field, field + i*j);
-        }
-        list.add(doc);
-      }
-      queriesToDocs.put("query" + i, list);
-    }
-
     var fileWriter = createTestFileWriter();
-    fileWriter.writeSolcolatorResults(queriesToDocs);
+    fileWriter.writeSolcolatorResults(QUERIES_TO_DOCS);
     fileWriter.close();
 
     String fileContent = Files.readString(Path.of(tmpDir.getAbsolutePath(), FILENAME), StandardCharsets.UTF_8);
     Type type = new TypeToken<Map<String, List<SolrInputDocument>>>() { }.getType();
     Gson gson = new Gson();
     Map<String, List<SolrInputDocument>> matches = gson.fromJson(fileContent, type);
-    assertEquals(numTestQueries, matches.size());
-    matches.forEach((k,v) -> assertEquals(numTestMatches, v.size()));
+    assertEquals(NUM_TEST_QUERIES, matches.size());
+    matches.forEach((k,v) -> assertEquals(NUM_TEST_MATCHES, v.size()));
   }
 
-  private FileWriter createTestFileWriter() throws IOException {
+  @Test
+  void shouldLogErrorIfWriteAfterClose() {
+    var logCaptor = LogCaptor.forClass(FileWriter.class);
+    var fileWriter = createTestFileWriter();
+    assertDoesNotThrow(fileWriter::close);
+    fileWriter.writeSolcolatorResults(QUERIES_TO_DOCS);
+    assertThat(logCaptor.getErrorLogs()).contains("Writing results to file is failed");
+  }
+
+  private FileWriter createTestFileWriter() {
     var fileWriter = new FileWriter();
     var outputConfig = new NamedList<String>();
     var path = Path.of(tmpDir.getAbsolutePath(), FILENAME);
     outputConfig.add(FILE_PATH, path.toString());
     outputConfig.add(FILE_FL, FIELD_LIST);
-    fileWriter.init(outputConfig);
+    try {
+      fileWriter.init(outputConfig);
+    } catch (IOException e) {
+      fail(e);
+    }
     return fileWriter;
   }
 
